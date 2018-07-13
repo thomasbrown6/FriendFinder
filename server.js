5cec22363ffd7f786143d9125f63d31697d72c78
Thomasbrown6 <Thomas.brown1125@gmail.com>
2018-07-13 09:50:45 -0400

    added static to get styles to work

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.static("app"));
 
 // ================================================================================
 // ROUTER
